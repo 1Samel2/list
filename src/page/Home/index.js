99aa304927fac27e4f4f50fb47cde5c92300a8cc
Samuel Guedes <91099488+1Samel2@users.noreply.github.com>
2022-09-11 19:15:14 -0300

    Finalizado

--- src/page/Home/index.js
+++ src/page/Home/index.js
@@ -1,20 +1,21 @@
 import React, { useState } from "react";
-import { BsTrash } from 'react-icons/bs'
-import { MdVerified } from 'react-icons/md'
+
 import {
   Container,
   HomePageContainer,
   InputAdd,
   Button,
+  DivToDolist,
   ListAddUl,
+  EmptyTaskList,
+  Check,
+  Trash
 } from "./styles";
 
 import { v4 as uuidv4 } from "uuid";
 
 export default function Home() {
-  const [list, setList] = useState([
-    { id: uuidv4(), task: "lavar o banheiro", finished: true },
-  ]);
+  const [list, setList] = useState([]);
 
   const [inputTask, setInputTask] = useState("");
 
@@ -23,8 +24,8 @@ export default function Home() {
   }
 
   function submitInput() {
-    if(inputTask){
-    setList([...list, { id: uuidv4(), task: inputTask, finished: false }]);
+    if (inputTask) {
+      setList([...list, { id: uuidv4(), task: inputTask, finished: false }]);
     }
   }
 
@@ -43,22 +44,22 @@ export default function Home() {
   return (
     <Container>
       <HomePageContainer>
-        <InputAdd onChange={enterInput} placeholder="o que tenho que fazer" />
+        <InputAdd onChange={enterInput} placeholder="Digita a sua tarefa para não esquecer" />
         <Button type="button" onClick={submitInput}>Adicionar</Button>
-        {
-          list.length > 0 ? (
-            <ul>
-              {list.map((item) => (
-                <ListAddUl isFinished={item.finished} key={item.id} >
-                  <MdVerified onClick={() => finalizarTarefa(item.id)} />
-                  <li>{item.task}</li>
-                  <BsTrash onClick={() => deleteItems(item.id)} />
-                </ListAddUl>
-              ))}
-            </ul>
-          ) : (
-            <h1>test</h1>
-          )}    
+          {
+            list.length > 0 ? (
+              <DivToDolist>
+                {list.map((item) => (
+                  <ListAddUl isFinished={item.finished} key={item.id} >
+                    <Check onClick={() => finalizarTarefa(item.id)} />
+                    <li>{item.task}</li>
+                    <Trash onClick={() => deleteItems(item.id)} />
+                  </ListAddUl>
+                ))}
+              </DivToDolist>
+            ) : (
+              <EmptyTaskList>Lista de tarefa vazia</EmptyTaskList>
+            )}
       </HomePageContainer>
     </Container>
   );
--- src/page/Home/styles.js
+++ src/page/Home/styles.js
@@ -1,9 +1,14 @@
 import Styled from "styled-components";
 
+import ToDoList from '../../assets/list.jpg'
+import { BsTrash } from 'react-icons/bs'
+import {AiOutlineCheckCircle} from 'react-icons/ai'
+
 export const Container = Styled.div`
-background: linear-gradient(90deg, #383838 0%, #000000 81.25%);
+background: url("${ToDoList}");
 width: 100vw;
-min-height: 100vh;
+height: 100vh;
+background-size: cover; 
 justify-content: center;
 text-align: center;
 align-items: center;
@@ -24,18 +29,19 @@ width: 342px;
 border-radius: 5px;
 outline: 0;
 margin-top: 41px;
-
+padding-left: 5px;
 ::placeholder{
 font-size: 15px;
 font-weight: 400;
 letter-spacing: 0px;
 text-align: left;
-padding-left: 20px;
+
 }
 `;
 
 export const Button = Styled.button`
-background: #8052EC;
+margin-bottom: 20px;
+background: #efa335;
 height: 40px;
 width: 130px;
 border-radius: 5px;
@@ -46,16 +52,32 @@ font-weight: bold;
 cursor: pointer;
 `;
 
+export const DivToDolist = Styled.ul`
+overflow-y: scroll;
+height: 498px;
+
+::-webkit-scrollbar{
+    background: white; 
+    border-radius: 5px;
+} 
+
+::-webkit-scrollbar-thumb {
+  background-color: #EFA335;    /* color of the scroll thumb */
+  border-radius: 3px;       /* roundness of the scroll thumb */
+  border: 1px solid black;  /* creates padding around scroll thumb */
+}
+` 
 export const ListAddUl = Styled.div`
 margin-top: 50px;
+margin-bottom: 50px;
 display: flex;
 justify-content: space-around;
 padding: 20px;
 margin-right: 35px;
 border-radius: 5px;
-background: ${props => props.isFinished ? '#E8FF8B' : '#E4E4E4'};
+background: ${props => props.isFinished ? '#efa335' : '#E4E4E4'};
 box-shadow:  0 6px 20px 0 rgba(0, 0, 0, 0.19);
-cursor: pointer;
+
 
 li{
     list-style: none;
@@ -63,3 +85,19 @@ li{
 }
 `;
 
+export const EmptyTaskList = Styled.p`
+padding: 100px;
+`
+
+export const Check = Styled(AiOutlineCheckCircle)`
+cursor: pointer;
+height: 20px;
+width: 20px;
+
+`
+
+export const Trash = Styled(BsTrash)`
+cursor: pointer;
+height: 20px;
+width: 20px;
+`
